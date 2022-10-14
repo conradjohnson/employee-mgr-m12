f12434aab6904e3cb0723b3fbd5a67e9acffaa21
conradjohnson <skaterjames@gmail.com>
2022-10-14 10:00:30 -0500

    lib framework and message test

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const inquirer = require('inquirer');
+const questions = require('./lib/questions');
+const header = require('./lib/header');
+const greeting = require('./lib/greeting');
 
-
-const questions = [
-{
-    type: 'list',
-    name: 'license',
-    message: 'Which Software License?',
-    choices: [
-        {value: 'MIT', name: "MIT"},
-        {value: 'GAGPL', name: "GNU AGPLv3"},
-        {value: 'GGPL', name: "GNU GPLv3"},
-        {value: 'MOZ', name: "Mozilla v2"},
-        {value: 'APA', name: "Apache v2"},
-        {value: 'ISC', name: "Internet Systems Consortium (ISC)"},
-        {value: 'UNL', name: "The Unlicense"},
-        {value: 'NO', name: "None"}
-      ]
-    ,
-}];
 
 const showMenu = () => {
     return inquirer.prompt(questions);
 }
 
 const main = async () => {
-    let greetString = `\x0AREADME.md generator\x0A===================\x0A\x0AWill auto-generate a README.md in 'readme' directory`;
-    console.log(greetString);
+   // console.log(greeting);
+    console.log(header());
     while (true) {
       await showMenu()
       .then(answers => {
