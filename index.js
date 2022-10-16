9eee578f5500046f49d7a10866bbcf18e5ac744b
conradjohnson <skaterjames@gmail.com>
2022-10-15 19:09:31 -0500

    separation of concerns from index.js to javascript lib.  Working for employee get and post.  Next is dynamic menu building for inquirer

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const inquirer = require('inquirer');
-const  {main_menu}  = require('./lib/menus');
+const  {main_menu, add_emp_menu}  = require('./lib/menus');
 const header = require('./lib/header');
 const greeting = require('./lib/greeting');
 const exit_message = require('./lib/exit_message');
-
+const sequelize = require('./config/connection');
+const { Department, Role, Employee } = require('./models');
+const employeeFunctions = require('./lib/employeeFunctions');
 
 const startMainMenu = async () => {
+ 
+ 
     const answer = await inquirer.prompt(main_menu);
     console.log(answer)
     return answer;
 }
+
+
 
 const main = async () => {
     let loop = true;
@@ -17,11 +23,29 @@ const main = async () => {
     console.log(header());
     while (loop) {
       await startMainMenu()
-      .then(answers => {
+      .then(async answers => {
         // Top level menu options back from main_menu
-        if (answers.emp_menu === 'get_emp'){
-          console.log("HERE's some employee info!");
+        if (answers.emp_menu != 'back' && answers.role_menu != 'back' && answers.dept_menu != 'back' ){
+          // that means we want some function to call in either one of the sub menus.
+          console.log("HERE's some action!");
+          if (answers.emp_menu){
+            console.log("Employee function");
+            let emp_menu_action = answers.emp_menu;
+            let emp_reply = await employeeFunctions(emp_menu_action);
+            //console.log(emp_reply);
+          }
+          if (answers.role_menu){
+            console.log("Role Functions");
+          }
+          if (answers.dept_menu){
+            console.log("Dept functions");
+          }
+
         }
+        // if (answers.emp_menu === 'post_emp') {
+        //   const new_employee =  inquirer.prompt(add_emp_menu);
+        //   console.log(new_employee);
+        // }
 
        if (answers.main_menu === 'exit'){
         loop = false;
@@ -30,7 +54,6 @@ const main = async () => {
   }
   // show exit greeting
   console.log(exit_message());
-
 
 };
 
