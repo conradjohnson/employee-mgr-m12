3994184266ac0a5dcf380a4beaa6b4ea362d94e1
conradjohnson <skaterjames@gmail.com>
2022-10-15 19:16:10 -0500

    cleaning up unused require

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const inquirer = require('inquirer');
-const  {main_menu, add_emp_menu}  = require('./lib/menus');
+const  {main_menu}  = require('./lib/menus');
 const header = require('./lib/header');
 const greeting = require('./lib/greeting');
 const exit_message = require('./lib/exit_message');
-const sequelize = require('./config/connection');
-const { Department, Role, Employee } = require('./models');
 const employeeFunctions = require('./lib/employeeFunctions');
 
 const startMainMenu = async () => {
- 
- 
     const answer = await inquirer.prompt(main_menu);
     console.log(answer)
     return answer;
@@ -20,33 +16,38 @@ const startMainMenu = async () => {
 const main = async () => {
     let loop = true;
    // console.log(greeting);
-    console.log(header());
-    while (loop) {
+   console.log(header());
+   
+   while (loop) {
       await startMainMenu()
       .then(async answers => {
+        
         // Top level menu options back from main_menu
         if (answers.emp_menu != 'back' && answers.role_menu != 'back' && answers.dept_menu != 'back' ){
           // that means we want some function to call in either one of the sub menus.
           console.log("HERE's some action!");
+          
+          // If we've selected some Employee Function, then let's go to that function.
           if (answers.emp_menu){
             console.log("Employee function");
             let emp_menu_action = answers.emp_menu;
             let emp_reply = await employeeFunctions(emp_menu_action);
             //console.log(emp_reply);
           }
+
+          // If user selected a Role Function
           if (answers.role_menu){
             console.log("Role Functions");
           }
+
+          // If user selected a Department Function
           if (answers.dept_menu){
             console.log("Dept functions");
           }
 
         }
-        // if (answers.emp_menu === 'post_emp') {
-        //   const new_employee =  inquirer.prompt(add_emp_menu);
-        //   console.log(new_employee);
-        // }
-
+       
+       // Exit Employee Manager (close program)
        if (answers.main_menu === 'exit'){
         loop = false;
        }
